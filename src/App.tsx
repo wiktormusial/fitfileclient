697720fa15c504fddfb64e35b5dae6edf50fb9ab
Wiktor Musial <wiktormusial@icloud.com>
2022-03-26 23:39:39 +0100

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import IndexView from "./views/IndexView";
+import DashboardView from "./views/DashboardView";
 
 function App() {
-  return <IndexView />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<IndexView />} />
+        <Route path="/dashboard" element={<DashboardView />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/views/DashboardView/index.tsx
@@ -0,0 +1,5 @@
+const DashboardView = () => {
+  return <h1>Dashboard</h1>;
+};
+
+export default DashboardView;
