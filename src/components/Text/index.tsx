1211d3235086fdcab24ca393b547ff06eaf3338c
Wiktor Musial <wiktormusial@icloud.com>
2022-03-27 12:56:32 +0200

    Change font

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -2,8 +2,11 @@ import styled from "styled-components";
 
 export const TextHeading = styled.h2`
   margin-top: 0;
+  color: #808080;
+  font-weight: bolder;
 `;
 
 export const TextLead = styled.h1`
   margin-bottom: 0;
+  font-weight: bold;
 `;
